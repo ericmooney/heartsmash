868e6ff30480569d1a3c6c2dd5735d6940d38064
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-26 16:18:14 -0700

    Basic rails and foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heartsmash::Application.config.secret_token = '9672aa19ee1ba4fd0aa5a0cb9c654466655e99707f1d5de6819c6051ecc6028572e7bb8b12d773d162dbc1edca839c44311d6dacdda059c96995882d0895880f'
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Heartsmash::Application.routes.draw do
+
+root :to => 'users#index'
+
+
+end
